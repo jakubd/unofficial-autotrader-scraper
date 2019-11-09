aaca16b7f494c2e0623ef04d65888bd1d5e90ab8
jakub <jakub.dalek@utoronto.ca>
2019-11-08 22:36:29 -0500

    some argparsing for the initial query

--- unofficialautotraderscraper/cliapp.py
+++ unofficialautotraderscraper/cliapp.py
@@ -1,26 +1,58 @@
+import argparse
 from unofficialautotraderscraper.autotraderca import query_to_search_url, db_write_for_search_query_url
 
-price_min = 500
-price_max = 13000
-price_range = "%d,%d" % (price_min, price_max)
+default_price = "500-1000"
+default_mileage = "10000-100001"
 
-mil_min = 10000
-mil_max = 100001
-mileage = "%d,%d" % (mil_min, mil_max)
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--price', type=str, default=default_price, help="Price range as a string for example: 1-1000.")
+parser.add_argument('-m', '--mileage', type=str,  default=default_mileage, help="Mileage range as a string for example: 1-1000.")
+parser.add_argument('-l', '--location', type=str,  default="M5H 2N2", help="Location as a Canadian postal code")
+parser.add_argument('-d', '--distance', type=int,  default="50", help="Distance in Km that you are searching from the --location")
+parser.add_argument('-at', '--automatictransmission', action='store_true',  default=True, help="Show only automatic transmission")
+parser.add_argument('-mt', '--manual-transmission', action='store_true',  default=False, help="Show only manual transmission")
+parser.add_argument('-o', '--output', type=str, default="cars.db", help="Output SQLite3 database name (default-  carss.db)")
 
-distance_from_postal_range = 50  # in km i think?
-postal = "M5H 2N2"               # defaults to center of toronto (city hall)
-transmission = "Automatic"
-car_status = "New-Used"
+args = parser.parse_args()
 
-max_results = 15    # or 10000
+def verify_range(givenstr):
+    if len(givenstr) <= 3:
+        return False
+    elif str(givenstr).find("-") < 0:
+        return False
 
-at_query = query_to_search_url(results_per_page=max_results, price_range=price_range,
-                               mileage_range=mileage, distance_from_me=distance_from_postal_range,
-                               postal=postal, transmission=transmission, car_status=car_status)
+    split = str(givenstr).split("-")
 
-print(at_query)
-#db_write_for_search_query_url(at_query, timeout=0.5)
+    if split is None:
+        return False
+    elif len(split) != 2:
+        return False
+
+    try:
+        int(split[0]) & int(split[1])
+    except ValueError:
+        return False
+
+    return True
+
+
+if not verify_range(args.price):
+    args.price = default_price
+
+if not verify_range(args.mileage):
+    args.mileage = default_mileage
+
+if args.manual_transmission:
+    transmission = "Manual"
+else:
+    transmission = "Automatic"
+
+at_query = query_to_search_url(results_per_page=10000, price_range=args.price,
+                               mileage_range=args.mileage, distance_from_me=args.distance,
+                               postal=args.location, transmission=transmission, car_status="New-Used")
+
+
+db_write_for_search_query_url(at_query, timeout=0.5, sqlite_fn=args.output)
 
 def stub():
     pass
